1b0e0fd08e9ca5f49ea549f5f63fa3db9511db23
AKB <akb@3azouz.net>
2019-01-30 11:26:26 +0100

    added batch-tokenize script

---
+++ scripts/batch-tokenize.py
@@ -0,0 +1,44 @@
+"""Tokenize a batch of .txt files with polyglot."""
+
+import os
+import functools
+import multiprocessing
+
+import witokit
+
+
+def _process(lowercase, output_dirpath, input_filepath):
+    output_filepath = os.path.join(
+        output_dirpath,
+        '{}.tkz.txt'.format(os.path.basename(input_filepath).split('.txt')[0]))
+    input = []
+    with open(input_filepath, 'r', encoding='utf-8') as input_stream:
+        for line in input_stream:
+            line = line.strip()
+            input.append(line)
+    with open(output_filepath, 'w', encoding='utf-8') as output_stream:
+        tokenized_txt = witokit.tokenize(' '.join(input), lowercase)
+        print(tokenized_txt, file=output_stream)
+    return input_filepath
+
+
+if __name__ == '__main__':
+    BATCH_DIRPATH = '/Users/akb/Github/witokit/data/'
+    OUTPUT_DIRPATH = '/Users/akb/Github/witokit/data/'
+    LOWERCASE = True
+    NUM_THREADS = 1
+
+    assert os.path.exists(BATCH_DIRPATH)
+    assert os.path.exists(OUTPUT_DIRPATH)
+
+    txt_filepaths = [os.path.join(BATCH_DIRPATH, filename) for filename in
+                     os.listdir(BATCH_DIRPATH) if not filename.startswith('.')]
+
+    file_num = 0
+    with multiprocessing.Pool(NUM_THREADS) as pool:
+        file_num += 1
+        process = functools.partial(_process, LOWERCASE, OUTPUT_DIRPATH)
+        for filepath in pool.imap_unordered(process, txt_filepaths):
+            print('Done processing file {}'.format(filepath))
+            print('Completed processing of {}/{} files'
+                  .format(file_num, len(txt_filepaths)))
